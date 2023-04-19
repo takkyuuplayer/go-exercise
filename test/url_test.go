9607435fb1403da69418ea5038f7b3f359d642a6
takkyuuplayer <takkyuuplayer@gmail.com>
2023-04-19 11:03:41 +0900

    Add url Parse test

---
+++ test/url_test.go
@@ -0,0 +1,35 @@
+package test
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParse(t *testing.T) {
+	t.Run("Parse", func(t *testing.T) {
+		u, _ := url.Parse("http://あ.example/い?search=う#え")
+
+		assert.Equal(
+			t,
+			&url.URL{
+				Scheme:      "http",
+				Host:        "あ.example",
+				Path:        "/い",
+				RawPath:     "/い",
+				RawQuery:    "search=う",
+				Fragment:    "え",
+				RawFragment: "え",
+			},
+			u,
+		)
+		assert.Equal(t, "http://%E3%81%82.example/%E3%81%84?search=う#%E3%81%88", u.String())
+		assert.Equal(t, "/%E3%81%84", u.EscapedPath())
+		assert.Equal(t, "search=%E3%81%86", u.Query().Encode())
+		assert.Equal(t, "%E3%81%88", u.EscapedFragment())
+
+		u.RawQuery = u.Query().Encode()
+		assert.Equal(t, "http://%E3%81%82.example/%E3%81%84?search=%E3%81%86#%E3%81%88", u.String())
+	})
+}
