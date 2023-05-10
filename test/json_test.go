b7e1a532f962bb1f9c02c611af3f84e2970ad1b4
takkyuuplayer <takkyuuplayer@gmail.com>
2023-05-10 10:55:19 +0900

    Remove

--- test/json_test.go
+++ test/json_test.go
@@ -7,6 +7,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestMarshal(t *testing.T) {
+	t.Run("- is ignored", func(t *testing.T) {
+		var params struct {
+			P1 bool
+			P2 bool `json:"-"`
+			P3 bool `json:""`
+		}
+		buf, err := json.Marshal(&params)
+		assert.Equal(t, `{"P1":false,"P3":false}`, string(buf))
+		assert.NoError(t, err)
+	})
+}
+
 func TestUnmarshal(t *testing.T) {
 	t.Run("null to boolean", func(t *testing.T) {
 		code := `{"bool_field": null}`
