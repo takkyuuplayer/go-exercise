2eec6a295877a53ee24853ce179ee66698fe6aef
takkyuuplayer <takkyuuplayer@gmail.com>
2025-09-26 22:12:26 +0900

    lint

--- test/time_test.go
+++ test/time_test.go
@@ -1,7 +1,6 @@
 package test
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
@@ -11,5 +10,5 @@ import (
 func TestTime(t *testing.T) {
 	t.Parallel()
 
-	assert.Equal(t, fmt.Sprintf("%s", time.Time{}), "0001-01-01 00:00:00 +0000 UTC")
+	assert.Equal(t, time.Time{}.String(), "0001-01-01 00:00:00 +0000 UTC")
 }
